3f0c94610efb5fa64ff13f2685ffb45125b54553
codica-studio <CodicaStudioDev@gmail.com>
2020-09-20 00:52:15 -0500

    Initial

---
+++ tests/Features/Methods/Builder.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Features\Methods;
+
+use App\User;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use stdClass;
+
+class Builder
+{
+    public function testGroupBy(): EloquentBuilder
+    {
+        return User::query()->groupBy('foo', 'bar');
+    }
+
+    public function testDynamicWhereAsString(): ?User
+    {
+        return (new User())->whereFoo('bar')->first();
+    }
+
+    public function testDynamicWhereMultiple(): ?User
+    {
+        return User::whereIdAndEmail(1, 'foo@example.com')->first();
+    }
+
+    public function testDynamicWhereAsInt(): ?User
+    {
+        return (new User())->whereFoo(1)->first();
+    }
+
+    public function testGetQueryReturnsQueryBuilder(): QueryBuilder
+    {
+        return User::getQuery();
+    }
+
+    public function testToBaseReturnsQueryBuilder(): QueryBuilder
+    {
+        return User::toBase();
+    }
+
+    public function testToBaseReturnsQueryBuilderAfterChain(): QueryBuilder
+    {
+        return User::whereNull('foo')
+            ->orderBy('fooBar')
+            ->toBase();
+    }
+
+    public function testQueryBuilderChainStartedWithGetQueryReturnsStdClass(): ?stdClass
+    {
+        return User::getQuery()
+        ->select('some_model.created')
+        ->where('some_model.some_column', '=', true)
+        ->orderBy('some_model.created', 'desc')
+        ->first();
+    }
+}
---
+++ tests/Features/Methods/Facades.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Features\Methods;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Redis;
+
+class Facades
+{
+    public function testEventAssertDispatched(): void
+    {
+        Event::assertDispatched('FooEvent');
+    }
+
+    public function testEventAssertDispatchedTimes(): void
+    {
+        Event::assertDispatchedTimes('FooEvent', 5);
+    }
+
+    public function testEventAssertNotDispatched(): void
+    {
+        Event::assertNotDispatched('FooEvent');
+    }
+
+    /** @return mixed[] */
+    public function testRedisFacadeLRangeMethod(): array
+    {
+        return Redis::lrange('some-key', 0, -1);
+    }
+
+    /** @return mixed[] */
+    public function testRedisFacadeConnection(): array
+    {
+        $redis = Redis::connection();
+
+        return $redis->lrange('some-key', 0, -1);
+    }
+
+    public function testRedisFacadeExpire(): bool
+    {
+        return Redis::expire('foo', 3);
+    }
+
+    /** @return mixed[] */
+    public function testRedisHmget(): array
+    {
+        return Redis::hmget('h', ['field1', 'field2']);
+    }
+}
