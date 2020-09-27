195a4418aedee74490db692ac3399c2347829aff
codica-studio <maintenance@codicastudio.com>
2020-09-26 23:26:22 -0500

    Update contact

--- src/Types/AbortIfFunctionTypeSpecifyingExtension.php
+++ src/Types/AbortIfFunctionTypeSpecifyingExtension.php
@@ -7,7 +7,7 @@ namespace NunoMaduro\Larastan\Types;
 /*
  * This file is part of Larastan.
  *
- * (c) Nuno Maduro <enunomaduro@gmail.com>
+ * (c) Nuno Maduro
  *
  *  For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
